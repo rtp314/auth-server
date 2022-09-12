6688b33cd56ac847d8ca1d094f8e27aab9823d03
Samuel Reed <samuelreed01@gmail.com>
2022-09-12 21:16:09 +0700

    style update

--- start.js
+++ start.js
@@ -4,6 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./server"));
-server_1.default.listen("8080", () => {
-    console.log("Server listening on port 8080");
+server_1.default.listen(process.env.PORT, () => {
+    console.log("Server listening on port " + process.env.PORT);
 });
